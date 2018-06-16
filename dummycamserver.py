d1f054dc8c0e1c524c01484ed7725a57cdc83d44
Christoph Stelz <mail@ch-st.de>
2018-06-17 00:58:06 +0200

    slight adaptions

--- dummycamserver.py
+++ dummycamserver.py
@@ -4,22 +4,22 @@ Name: Camserver
 Purpose: Serve raw bayer frames over HTTP
 """
 
-from picamera import PiCamera
-from picamera.array import PiBayerArray
-
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import argparse
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 class CamServer_CaptureHandler():
-    def __init__(self):
-        self.cam = PiCamera()
-        self.arr = PiBayerArray(self.cam)
+    def __init__(self, imagefile):
+        self.image = plt.imread(imagefile)
+        assert self.image.shape[2] == 3 # Assert 3 channels
+
 
     def capture(self):
-        logging.info('Capturing frame from camera')
-        self.cam.capture(self.arr, 'jpeg', bayer=True)
-        return self.arr
+        x = self.image.astype(np.uint16)
+        return x
 
 
 class CamServer_RequestHandler(BaseHTTPRequestHandler):
@@ -48,20 +48,22 @@ class CamServer_RequestHandler(BaseHTTPRequestHandler):
             msg = self.server.capture_handler.capture()
 
             self.send_header('Content-Type', 'application/octet-stream')
-            self.send_header('X-Array-Width', msg.shape[1])
-            self.send_header('X-Array-Height', msg.shape[0])
+            self.send_header('X-Array-Width', msg.array.shape[1])
+            self.send_header('X-Array-Height', msg.array.shape[0])
             self.send_header('X-Array-Channels', 1)
-            self.send_header('X-Array-Type', msg.dtype)
+            self.send_header('X-Array-Type', msg.array.dtype)
 
             self.end_headers()
 
-            self.wfile.write(msg[:, :, 0].tobytes())
+            self.wfile.write(msg.array[:, :, 0].tobytes())
 
 if __name__ == '__main__':
     """ Parse commandline arguments """
     parser = argparse.ArgumentParser(description='Capture bayer frames and serve them over HTTP')
     parser.add_argument('--address', type=str, help='Binding address for the HTTP server', default='0.0.0.0')
     parser.add_argument('--port', type=int, help='Port the HTTP server will listen on', default=3000)
+
+    parser.add_argument('imagefile', type=str, help='Image file the camserver will server')
 
     args = parser.parse_args()
 
@@ -72,7 +74,7 @@ if __name__ == '__main__':
     """ Run the server """
     server_address = (args.address, args.port)
     httpd = HTTPServer(server_address, CamServer_RequestHandler)
-    httpd.capture_handler = CamServer_CaptureHandler()
+    httpd.capture_handler = CamServer_CaptureHandler(args.imagefile)
     logging.info("Serving on http://{}:{}".format(server_address[0], server_address[1]))
 
     httpd.serve_forever()
